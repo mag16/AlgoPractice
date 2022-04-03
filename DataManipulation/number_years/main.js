7cad585b1cb74396b3960b9b9c85c9f4681b2e7c
Marco A. Guzman <marcoguzman16@gmail.com>
2022-04-03 13:49:22 -0700

    sunday push

---
+++ DataManipulation/number_years/main.js
@@ -0,0 +1,46 @@
+//DS1 must match DS2 or else its not valid (State  Farm).  Print true if valid else false.
+
+const db1 = [
+  { year: "2000", state: "Rio", month: "November", number: 18 },
+  { year: "2002", state: "Perambuco", month: "February", number: 64 },
+  { year: "2001", state: "Mato Grasso", month: "March", number: 112 },
+  { year: "2003", state: "Roraima", month: "January", number: 547 },
+  { year: "2002", state: "Maranhoo", month: "July", number: 4 },
+  { year: "2003", state: "Rio", month: "March", number: 9 },
+  { year: "2000", state: "Roraima", month: "October", number: 25 },
+  { year: "2001", state: "Paraiba", month: "January", number: 11 },
+];
+
+const db2 = [
+  { year: "2002", number: 68 },
+  { year: "2000", number: 43 },
+  { year: "2003", number: 556 },
+  { year: "2001", number: 123 },
+];
+
+
+
+//variable sumPerYear returns the sum per year summing up each month and returning it per month.
+const sumPerYear = db1.reduce((acc, cur) => {
+  acc[cur.year] = acc[cur.year] + cur.number || cur.number; // increment or initialize to cur.number
+  return acc;
+}, {});
+
+console.log(sumPerYear)
+
+
+
+//console.log(compare(sumPerYear, db2));
+
+const isEqual = (sumPerYear, db2) => {
+  if (sumPerYear.length !== db2.length) {
+    return false;
+  }
+  const toStr = ({ year, number }) => `${year}-${number}`;
+  const a1 = Array.from(sumPerYear, toStr);
+  const a2 = Array.from(number, toStr);
+  return a1.every((item) => a2.includes(item));
+};
+
+
+console.log(isEqual(sumPerYear, db2));
