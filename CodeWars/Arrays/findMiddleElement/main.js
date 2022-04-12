cfee7360878f1e854febe034ecbd54671a3733d8
Marco A. Guzman <marcoguzman16@gmail.com>
2022-04-12 16:11:18 -0700

    array probs code wars

---
+++ CodeWars/Arrays/findMiddleElement/main.js
@@ -0,0 +1,31 @@
+/*
+As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+For example:
+
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+Another example (just to make sure it is clear):
+
+gimme([5, 10, 14]) => 1
+*/
+
+
+
+
+
+function gimme(triplet) {
+  //track the index of each num first
+  let tripletCopy = [];
+
+  for (let i of triplet) {
+    tripletCopy.push(i);
+  }
+
+  tripletCopy.sort((a, b) => a - b);
+
+  return triplet.indexOf(tripletCopy[1]);
+}
