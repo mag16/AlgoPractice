93fb3106e9e8fa3d72c0a2a25aa416fb27da6d5c
Marco A. Guzman <marcoguzman16@gmail.com>
2022-04-12 13:39:21 -0700

    reversed phrase in place working soln

---
+++ CodeWars/Strings/reverseStrInPlace/main.js
@@ -0,0 +1,15 @@
+/*
+Complete the function that accepts a string parameter, and reverses each word in the string.All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps"
+*/
+
+function reverseWords(str) {
+  // Go for it
+  let reverseWordArr = str
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""));
+  return reverseWordArr.join(" ");
+}
