ced1920cce16ca89420c206176bd2ed2fc653fe7
Marco A. Guzman <marcoguzman16@gmail.com>
2022-06-06 19:36:11 -0700

    kata domainName()

---
+++ CodeWars/Strings/ExtractDomainfromUrl/main.js
@@ -0,0 +1,19 @@
+/*
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet"
+
+*/
+
+function domainName(url) {
+  //your code here
+  let sourceString = url
+    .replace("http://", "")
+    .replace("https://", "")
+    .replace("www.", "")
+    .split(/[/?#]/)[0];
+  let domain = sourceString.split(".")[0];
+  return domain;
+}
