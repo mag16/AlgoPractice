79587c7e08f64fde82d880418b35d115d9d917f8
Marco A. Guzman <marcoguzman16@gmail.com>
2022-06-26 18:10:25 -0700

    two sticks init

---
+++ interviews/woodenSticks/main.js
@@ -0,0 +1,39 @@
+/* 
+Microsoft codility Q1
+
+There are two wooden sticks of lengths A and B respectively.
+Each of them can be cut into shorter sticks of interger lengths.  
+Our goal is to construct the largest possible square.  In order to do this,
+we want to cut the sticks in such a way as to achieve four sticks of the same
+length (note gthat there can be some leftover pieces).  What is the longest
+side of the square that we can achieve?
+
+Write a function:
+    function solution(A,B);
+
+that, given two integers A,B, returns trhe side of the length
+of the largest square that we can obtain.  If its not possible to create
+any square, the function should return 0.
+
+Examples:
+1. Given A = 10, B = 21, the function should return 7. We can split
+the second stick into three sticks of length 7 and shorten the first stick by 3.
+
+2. Given A = 13, B = 11, the function should return 5.  We can cut two sticks of length
+5 from each of the sticks.
+
+3. Given A = 2, B = 1, the function should return 0. It is not possible to make any square from the given sticks.
+
+4.Given A = 1, B = 8, the function should return 2.  We can cut the stick B into four parts.
+
+Write an efficient algorithm for the following assumptions:
+    * A and B are integers within the range [1.. 1,000,000,000]
+*/
+
+function solution(A, B) {
+    
+
+
+
+    
+}
